1404eba72945e324ca66a19bb799126c64263162
PAVAN <122382391+babupavan@users.noreply.github.com>
2024-08-04 14:55:22 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://13.232.205.245:5000",
   withCredentials: true,
   timeout: 10000,
   delayed: true,
